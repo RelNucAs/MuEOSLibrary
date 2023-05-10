48b436af795d8cf7826643c485ed252963b52d97
LeonardoChiesa <leonardo.chiesa@studenti.unitn.it>
2023-05-10 17:04:13 +0200

    Addition of functions for anti(neutrino) EOS

--- include/eos_neutrinos.hpp
+++ include/eos_neutrinos.hpp
@@ -1,14 +1,41 @@
+#ifndef EOS_NU_H
+#define EOS_NU_H
+
 /* NEUTRINO EOS AT EQUILIBRIUM WITH RADIATION (neutrinos degeneracy parameter is equal to minus anti-neutrinos degeneracy parameter) */
-#pragma once
 
-//fraction of neutrinos (yn)
-double nu_fraction(double rho, double T, double eta);
+#include "constants.hpp"
+#include "tools/fermi_integrals.hpp"
+#include "eos_neutrinos.hpp"
 
-//fraction of antineutrinos (yan)
-double anu_fraction(double rho, double T, double eta);
+using namespace constants;
 
-//energy per baryon of neutrinos (en)
-double nu_energy(double rho, double T, double eta);
+/* Compute constant in front of neutrino EOS once for all */
+const double nu_const = 4. * pi / pow(h*c*1.e13,3.); // MeV-3 fm-3
 
-//energy per baryon of antineutrinos (ean)
-double anu_energy(double rho, double T, double eta);
+/* Input parameters:
+ *    - number density                nb [fm-3]
+ *    - temperature                   t  [MeV]
+ *    - neutrino degeneracy parameter eta
+*/
+
+// Number fraction of neutrinos (yn)
+double nu_fraction(const double nb, const double T, const double eta) {
+	return nu_const * pow(T,3.) * Fermi_integral_p2(eta) / nb;
+}
+
+// Number fraction of antineutrinos (yan)
+double anu_fraction(const double nb, const double T, const double eta) {
+	return nu_const * pow(T,3.) * Fermi_integral_p2(-eta) / nb;
+}
+
+// Energy per baryon of neutrinos (en)
+double nu_energy(const double nb, const double T, const double eta) {
+        return nu_const * pow(T,4.) * Fermi_integral_p3(eta) / nb; // MeV/baryon
+}
+
+// Energy per baryon of antineutrinos (ean)
+double anu_energy(const double nb, const double T, const double eta) {
+        return nu_const * pow(T,4.) * Fermi_integral_p3(-eta) / nb; // MeV/baryon
+}
+
+#endif
